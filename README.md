ccd701ba31f70376d72a13f587b36e214187ff15
Maciej Sawicki <saw.maciej@gmail.com>
2018-03-04 19:08:47 +0100

    Added website files

---
+++ README.md
@@ -0,0 +1,2 @@
+# cms-cm-warszawska
+Content source serving as cms for static website
---
+++ cms-cm-warszawska.js
@@ -0,0 +1,216 @@
+var cmsTexts = {
+  
+  numerTelefonu: 
+  "85 875 53 55",
+  
+  numerTelefonuBezSpacji: 
+  "858755355",
+  
+  numerTelefonuKomorka: 
+  "+48 512 677 311",
+  
+  numerTelefonuKomorkaBezSpacji: 
+  "+48512677311",
+  
+  numerTelefonuMedstyl: 
+  "662 149 977",
+  
+  numerTelefonuMedstylBezSpacji: 
+  "662149977",
+  
+  aktualnoscPierwszaTytul: 
+  "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
+  
+  aktualnoscPierwszaData: 
+  "Czwartek, 15 lutego 2018",
+  
+  aktualnoscPierwszaTekst: 
+  "Prosimy o rejestrację telefoniczną",
+  
+  aktualnoscDrugaTytul: 
+  "Promocja! Powiększanie ust - super cena od 600zł",
+  
+  aktualnoscDrugaData: 
+  "Wtorek, 14 lutego 2017",
+  
+  aktualnoscDrugaTekst: 
+  "Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
+  
+  dodatkoweKontaktyAkapitPierwszy:
+  "Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
+  
+  dodatkoweKontaktyAkapitDrugi:
+  "Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
+  
+  dodatkoweKontaktyAkapitTrzeci:
+  "Informacja medyczne całodobowo | tel. 194 39",
+  
+  kontaktGodzinyOtwarcia: 
+  "PN - PT  w godzinach 8.00 - 18.00",
+  
+  rodzinnaGodzinyOtwarcia:
+  "Poradnia jest czynna od poniedziałku do piątku w godzinach 8.00 - 18.00. Zapisy na wizyty w godz. 8.00- 17.00 telefonicznie, osobiście, za pośrednictwem osób trzecich.",
+  
+  rodzinnaLekarz1:
+  "lek. med. Elżbieta Malinowska - specjalista medycyny rodzinnej, pediatra",
+  
+  rodzinnaLekarz2:
+  "lek. med. Patrycja Budna - specjalista medycyny rodzinnej",
+  
+  rodzinnaLekarz3:
+  "lek. med. Wojciech Budny - specjalista chorób wewnętrznych",
+  
+  rodzinnaLekarz4:
+  "piel. Irena Dawidowicz - specjalista pielęgniarstwa pediatrycznego",
+  
+  rodzinnaLekarz5:
+  "mgr piel. Urszula Moniuszko - specjalista pielęgniarstwa anestezjologicznego i intensywnej opieki",
+  
+  rodzinnaLekarz6:
+  "piel. Katarzyna Jasińska",
+  
+  rodzinnaLekarz7:
+  "mgr położna Katarzyna Kitlasz - Baczyńska",
+  
+  rodzinnaLekarz8:
+  "",
+  
+  rodzinnaLekarz9:
+  "",
+  
+  personelRamkaOpiekaWSwieta: 
+  "W godzinach 18.00 - 8.00 od poniedziałku do piątku, w weekendy oraz w dni wolne ustawowo i świąteczne bezpłatną opiekę nad naszymi pacjentami przejmuje Samodzielny Szpital Miejski im. PCK ul.Sienkiewicza 79 w Białymstoku tel. 85 652 25 05 oraz SPZOZ Wojewódzki Szpital Zespolony ul. Skłodowskiej 26 w Białymstoku tel. 85 748 81 26",
+  
+  usgLekarz1__imie:
+  "Lek. med. Marta Martynowicz",
+  
+  usgLekarz1__opis:
+  "Badania niemowląt, dzieci i dorosłych",
+  
+  usgLekarz1__godziny:
+  "",
+  
+  usgLekarz2__imie:
+  "Lek. med. Monika Plona-Herbst",
+  
+  usgLekarz2__opis:
+  "Badania niemowląt, dzieci i dorosłych",
+  
+  usgLekarz2__godziny:
+  "",
+  
+  usgLekarz3__imie:
+  "Lek. med. Julia Rutkowska",
+  
+  usgLekarz3__opis:
+  "Specjalista radiologii i diagnostyki obrazowej. Badania dzieci i dorosłych",
+  
+  usgLekarz3__godziny:
+  "",
+  
+  usgLekarz4__imie:
+  "",
+  
+  usgLekarz4__opis:
+  "",
+  
+  usgLekarz4__godziny:
+  "",
+  
+  usgCennik__wiersz1__tytul:
+  "USG brzucha",
+  
+  usgCennik__wiersz1__cena:
+  "70 zł",
+  
+  usgCennik__wiersz2__tytul:
+  "USG tarczycy",
+  
+  usgCennik__wiersz2__cena:
+  "60 zł",
+  
+  usgCennik__wiersz3__tytul:
+  "USG piersi ",
+  
+  usgCennik__wiersz3__cena:
+  "80 zł",
+  
+  usgCennik__wiersz4__tytul:
+  "USG układu moczowego",
+  
+  usgCennik__wiersz4__cena:
+  "70 zł",
+  
+  usgCennik__wiersz5__tytul:
+  "USG brzucha z oceną gruczołu krokowego",
+  
+  usgCennik__wiersz5__cena:
+  "80 zł",
+  
+  usgCennik__wiersz6__tytul:
+  "USG gruczołu krokowego",
+  
+  usgCennik__wiersz6__cena:
+  "60 zł",
+  
+  usgCennik__wiersz7__tytul:
+  "USG węzłów chłonnych",
+  
+  usgCennik__wiersz7__cena:
+  "60 zł",
+  
+  usgCennik__wiersz8__tytul:
+  "USG ślinianek",
+  
+  usgCennik__wiersz8__cena:
+  "60 zł",
+  
+  usgCennik__wiersz9__tytul:
+  "USG tkanek miękkich",
+  
+  usgCennik__wiersz9__cena:
+  "60 zł",
+  
+  usgCennik__wiersz10__tytul:
+  "USG Doppler aorty i tętnic biodrowych",
+  
+  usgCennik__wiersz10__cena:
+  "120 zł",
+  
+  usgCennik__wiersz11__tytul:
+  "USG Doppler żył lub tętnic kończyn dolnych",
+  
+  usgCennik__wiersz11__cena:
+  "-",
+  
+  usgCennik__wiersz12__tytul:
+  "– obie kończyny dolne",
+  
+  usgCennik__wiersz12__cena:
+  "110 zł",
+  
+  usgCennik__wiersz13__tytul:
+  "– jedna kończyna dolna",
+  
+  usgCennik__wiersz13__cena:
+  "80 zł",
+  
+  usgCennik__wiersz14__tytul:
+  "USG Doppler tętnic szyjnych i kręgowych",
+  
+  usgCennik__wiersz14__cena:
+  "90 zł",
+  
+  usgCennik__wiersz15__tytul:
+  "USG Doppler tętnic nerkowych",
+  
+  usgCennik__wiersz15__cena:
+  "140 zł",  
+  
+  usgCennik__wiersz16__tytul:
+  "",
+  
+  usgCennik__wiersz16__cena:
+  "",  
+  
+}
